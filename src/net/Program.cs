348d725d6e33d0c1804af82067b42849c8d27a1d
Jon Gallant <jon@jongallant.com>
2020-06-29 10:58:33 -0700

    Add k8s files

--- src/net/Program.cs
+++ src/net/Program.cs
@@ -1,19 +1,32 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Azure.Identity;
 using Azure.Security.KeyVault.Keys;
+using System.Linq;
 
 namespace AzureCliCredentialContainer
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            var cred = new AzureCliCredential();
-            var client = new KeyClient(new Uri("https://jongkv.vault.azure.net"), cred);
-            var key = await client.GetKeyAsync("key1");
+            Directory.GetFiles("/root/.azure").ToList().ForEach(f => Console.WriteLine(f));
+            /*
+                        try
+                        {
+                            var cred = new AzureCliCredential();
+                            var client = new KeyClient(new Uri("https://jongkv.vault.azure.net"), cred);
+                            var key = await client.GetKeyAsync("key1");
+                            Console.WriteLine(key.Value.Name);
 
-            Console.WriteLine(key.Value.Name);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+                        */
+            while (true) { }
         }
     }
 }
